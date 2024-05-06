e7cab22e03d19039ce4c468aec8d784d990f6056
my name <blueshaperd3@gmail.com>
2024-05-07 02:36:42 +0500

    yaa img hai jispar click karna sai img change hogi

--- app.js
+++ app.js
@@ -3,7 +3,19 @@ document.getElementById("myButton1").addEventListener("click", function(){
     alert("Button Clicked")
 }); 
 
+
 // yaa button par hover active aur clicked hota hai
 document.getElementById("myButton2").addEventListener("click", function(){
     this.classList.add("clicked")
 })
+
+
+// yaa image button hai jis par click karna sai image change ho jai gi
+function changeImage(){
+    var image = document.getElementById("myImage");
+    if (image.src.match("image/nature-319.jpg")){
+        image.src = "image/th.jpeg";
+    } else {
+        image.src = "image/nature-319.jpg";
+    }
+}
