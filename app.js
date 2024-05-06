3b479ec466dcf71165b5c89683c367bde3ba7271
my name <blueshaperd3@gmail.com>
2024-05-07 01:58:34 +0500

    is button par hover active aur clicked hota hai

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-document.getElementById("myButton")
-.addEventListener("click", function(){
+// Yaa ek simple button function hai
+document.getElementById("myButton1").addEventListener("click", function(){
     alert("Button Clicked")
 }); 
 
+// yaa button par hover active aur clicked hota hai
+document.getElementById("myButton2").addEventListener("click", function(){
+    this.classList.add("clicked")
+})
